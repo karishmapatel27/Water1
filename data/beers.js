583d62ae4559be2e57b99bb3ee6d735919b35502
Bhavisha Das <bhivxx@gmail.com>
2020-09-11 13:34:56 +1200

    vids

--- data/beers.js
+++ data/beers.js
@@ -1,40 +1,49 @@
 export default {
-  products: [
-    {
-      id: 1,
-      name: 'Face mask',
-      price: 3,
-      // image: 'https://s3.amazonaws.com/protocols-files/public/fc9fa8fe3c4b76b388ce54ec9d6b0876ca2ef2e5fd251bb4daadb07095140737/b49udtje.png'
-      image: 'https://images-na.ssl-images-amazon.com/images/I/61MQy3POZOL._SL1000_.jpg'
-    },
-    {
-      id: 2,
-      name: 'Hand Sanitizer',
-      price: 10,
-      // image: 'https://media.gettyimages.com/vectors/sanitize-and-disinfect-hands-vector-id1221041058?s=2048x2048'
-      image: 'https://images-na.ssl-images-amazon.com/images/I/51v4rFT8TNL._SL1100_.jpg'
-    },
-    {
-      id: 3,
-      name: 'Hand Wash',
-      price: 8,
-      // image: 'https://i.insider.com/57d85e9d077dccf2018b5941',
-      // secondImage: 'https://media.gettyimages.com/vectors/black-and-white-washing-hands-vector-id1203067645?s=2048x2048'
-      image: 'https://i.ebayimg.com/images/g/DI8AAOSwOzleGD8g/s-l1600.jpg',
-      video: 'https://www.youtube.com/embed/77IZttD_pU8'
-    },
-    {
-      id: 4,
-      name: 'Gloves',
-      price: 5,
-      // image: 'https://oursafety.info/wp-content/uploads/2020/04/38_EN_WEARING-THE-GLOVES_17-04-2020-4.png'
-      image: 'https://images-na.ssl-images-amazon.com/images/I/61YO%2BaQShHL._AC_SL1000_.jpg'
-    },
-    {
-      id: 5,
-      name: 'Antibacterial Wipes',
-      price: 6,
-      // image: 'https://media.gettyimages.com/vectors/disinfecting-wipes-coronavirus-covid19-icon-vector-id1216058170?s=2048x2048'
-      image: 'https://images.officeworks.com.au/api/2/img/https://s3-ap-southeast-2.amazonaws.com/wc-prod-pim/JPEG_1000x1000/LE3042247_dettol_2_in_1_anti_bacterial_hand_and_surface_wipes_15_pack.jpg/resize?size=706&auth=MjA5OTcwODkwMg__'
-    }]
-}
+	products: [
+		{
+			id: 1,
+			name: 'Face mask',
+			price: 3,
+			// image: 'https://s3.amazonaws.com/protocols-files/public/fc9fa8fe3c4b76b388ce54ec9d6b0876ca2ef2e5fd251bb4daadb07095140737/b49udtje.png'
+			image:
+				'https://images-na.ssl-images-amazon.com/images/I/61MQy3POZOL._SL1000_.jpg',
+			video: 'https://www.youtube.com/embed/2xLjCfmx0iE',
+		},
+		{
+			id: 2,
+			name: 'Hand Sanitizer',
+			price: 10,
+			// image: 'https://media.gettyimages.com/vectors/sanitize-and-disinfect-hands-vector-id1221041058?s=2048x2048'
+			image:
+				'https://images-na.ssl-images-amazon.com/images/I/51v4rFT8TNL._SL1100_.jpg',
+			video: 'https://www.youtube.com/embed/4xC-_7ZiQoY',
+		},
+		{
+			id: 3,
+			name: 'Hand Wash',
+			price: 8,
+			// image: 'https://i.insider.com/57d85e9d077dccf2018b5941',
+			// secondImage: 'https://media.gettyimages.com/vectors/black-and-white-washing-hands-vector-id1203067645?s=2048x2048'
+			image: 'https://i.ebayimg.com/images/g/DI8AAOSwOzleGD8g/s-l1600.jpg',
+			video: 'https://www.youtube.com/embed/77IZttD_pU8',
+		},
+		{
+			id: 4,
+			name: 'Gloves',
+			price: 5,
+			// image: 'https://oursafety.info/wp-content/uploads/2020/04/38_EN_WEARING-THE-GLOVES_17-04-2020-4.png'
+			image:
+				'https://images-na.ssl-images-amazon.com/images/I/61YO%2BaQShHL._AC_SL1000_.jpg',
+			video: 'https://www.youtube.com/embed/YOWa__qWp6U',
+		},
+		{
+			id: 5,
+			name: 'Antibacterial Wipes',
+			price: 6,
+			// image: 'https://media.gettyimages.com/vectors/disinfecting-wipes-coronavirus-covid19-icon-vector-id1216058170?s=2048x2048'
+			image:
+				'https://images.officeworks.com.au/api/2/img/https://s3-ap-southeast-2.amazonaws.com/wc-prod-pim/JPEG_1000x1000/LE3042247_dettol_2_in_1_anti_bacterial_hand_and_surface_wipes_15_pack.jpg/resize?size=706&auth=MjA5OTcwODkwMg__',
+			video: 'https://www.youtube.com/embed/3tEu-JJh4WA',
+		},
+	],
+};
