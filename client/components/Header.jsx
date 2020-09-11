786fe3abf31735fe20a682ba1e656154554c4c01
Bhavisha Das <bhivxx@gmail.com>
2020-09-11 12:22:08 +1200

    cart button

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link, Router } from 'react-router-dom';
 
 class Header extends React.Component {
-  render () {
-    return (
-    <>
-    <input type="text" placeholder={'Search...'}/>
-    </>
-    )
-  }
+	render() {
+		return (
+			<>
+				<input type='text' placeholder={'Search...'} />
+				<Link to='/cart'>
+					<button>Cart</button>
+				</Link>
+			</>
+		);
+	}
 }
 
-export default Header
+export default Header;
